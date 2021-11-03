adbb6dda459d16b0595f4124c757d2c592ab926c
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2021-11-02 21:30:38 -0700

    changing branch

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+
+var telegramUrl = "https://api.telegram.org/bot" + token + "/makePost";
+var chat_id = "1182469925"
+var chatSelf = "2073270337"
+var textTest = "ITS LITT"
+var photoTest = "https://www.tradingview.com/x/BBM1NSQW/"
+var token = config.MY_API_TOKEN;
+var key = config.SECRET_API_KEY;
+
+
+var telAPI = "https://api.telegram.org/bot" + token + key + "/sendPhoto?chat_id=" + chatSelf + "&photo=" + photoTest + "&caption=" + textTest;
+
+async function makePost() {
+  const data = {
+      name: 'Tester Bot',
+      description: 'Testing J Tester',
+      public: false
+  }
+ 
+  try {
+      let response = await fetch(telAPI,{
+          headers:{
+              'Content-Type' : 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(data),
+          
+          
+      });
+      if (response.ok) {
+          console.log("Posting To Telegram")
+          console.log(respone)
+          
+      } else {
+          console.log("Something went wrong")
+          console.log(response)
+      }
+  } catch (err) {
+      console.log(err);
+  }
+}
+
+makePost();
