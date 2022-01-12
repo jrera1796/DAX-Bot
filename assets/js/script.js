b344a047b8b140bcd1caef554e1633eeca52b882
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-01-11 23:59:40 -0800

    Added express session handlebars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-
-var telegramUrl = "https://api.telegram.org/bot" + token + "/makePost";
-var chat_id = "1182469925"
-var chatSelf = "2073270337"
-var textTest = "ITS LITT"
-var photoTest = "https://www.tradingview.com/x/BBM1NSQW/"
-var token = config.MY_API_TOKEN;
-var key = config.SECRET_API_KEY;
-
-
 var telAPI = "https://api.telegram.org/bot" + token + key + "/sendPhoto?chat_id=" + chat_id + "&photo=" + photoTest + "&caption=" + textTest;
 
 async function makePost() {
