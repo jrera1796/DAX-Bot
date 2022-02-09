726e0568f046b3b0d5931867f5ca0a7108209b1e
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-02-09 10:15:48 -0800

    Testing chart image api

--- routes/api/signal-routes.js
+++ routes/api/signal-routes.js
@@ -5,12 +5,23 @@ const token = process.env.API_TOKEN
 const bot = new TelegramBot(token, { polling: true });
 const dateFormat = require('../../utils/dateFormat');
 
+function chartIMG(){
+  console.log('Fetching chart image')
+fetch(
+`https://api.chart-img.com/v1/tradingview/advanced-chart?symbol=OANDA:DE30EUR&interval=1h&width=500&height=300&key=${process.env.IMG_KEY}`)
+.then(CurrentChart=> {
+  return CurrentChart
+}
+)
+}
+
 router.post('/', (req, res) => {
   Signals.create({
     post_text: req.body.post_text,
     calledAt: req.body.calledAt
   })
     .then(dbUserData => {
+      chartIMG();
       const calledAtNew = dateFormat(req.body.calledAt)
       //Import bot from a different folder and make it private
       bot.sendMessage(1182469925, `Relative Session Time\n${calledAtNew}\n\n\n${req.body.post_text},`)
