8f1e151e67d33f13d15e18cf536906fedebef536
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2022-08-12 10:07:07 -0700

    Telegram command capture and error handling

--- routes/api/telegram-routes.js
+++ routes/api/telegram-routes.js
@@ -4,58 +4,86 @@ const router = require('express').Router();
 const bot = new TelegramBot(token, { polling: true });
 const dateFormat = require('../../utils/dateFormat');
 const Signals = require('../../models/Signals');
-
+let er = 0;
 require('dotenv').config
-//Move bot functions out of signal-routes and into here
 
-Signals.findAll({
-  attributes: ['id', 'exchange', 'ticker', 'notes', 'image', 'createdAt']
-})
-  .then(data => {
-    console.log(data, 'From Signal')
-    bot.setMyCommands(['Current Analysis', 'View the most recent Analysis'], (options) => {
-
-    })
-  })
-  .catch(
-    err => { console.log("Signals Table not populated") }
-  )
-
-
-
-bot.on('message', (msg) => {
+bot.onText(/(\a)/, (msg, match) => {
   const chatId = msg.chat.id;
+  const sw = match.input
 
-  if (msg.text === 'Current Analysis') {
-    Signals.findAll({
-      attributes: ['id', 'exchange', 'ticker', 'notes', 'createdAt']
-    }).then(data => {
-      return data
-    })
-      .then(res => {
+  switch (sw) {
 
-        let r = res.slice(-1)
-        let data = r[0].dataValues
-        let time = dateFormat(data.createdAt)
-        // bot.sendPhoto(chatId, `${data.image}`)
-        bot.sendMessage(chatId,
-          `${data.exchange}:${data.ticker}\n\n${data.notes}\n${time}\n\n`
-          //Try to clean this up a bit.
-        )
-      })
-      .catch(
-        err => { console.log(err) }
-      )
+    case 'Current Analysis':
+    case 'ca':
+    case '/ca':
+      currentAnalysis(chatId, -1)
+      break;
+
+    case 'Previous Analysis':
+    case 'pa':
+    case '/pa':
+      currentAnalysis(chatId, -2)
+      break;
+    case null:
+    case undefined:
+    case '/dax':
+      er++
+      if (er === 1) {
+        errorMessages(chatId)
+      }
+      break;
+
+    //Tests and other responses
+
+    case 'Dax Dice':
+    case '/dice':
+      diceGame(chatId);
+      break;
   }
-  else {
-    for (i = 0; i > 5; i++) {
-      bot.sendMessage(chatId, "I don't understand");
-      i = 0;
-    }
-    return
-  }
-
+  return;
 });
+
+function currentAnalysis(cn, cd) {
+  //Chat Number, Current Day
+  Signals.findAll({
+    attributes: ['id', 'exchange', 'ticker', 'notes', 'createdAt']
+  }).then(data => {
+    return data
+  })
+    .then(res => {
+      let r = res.slice(cd)
+      let data = r[0].dataValues
+      let time = dateFormat(data.createdAt)
+      bot.sendMessage(cn,
+        `${data.exchange}:${data.ticker}\n\n${data.notes}\n${time}`
+      )
+    })
+    .catch(
+      err => { console.log(err) }
+    )
+}
+
+function errorMessages(n) {
+  bot.sendMessage(n,
+    `I'm not sure I understood that.`
+  )
+  er = 0;
+}
+
+function diceGame(n) {
+  let dv1; let dv2;
+  bot.sendDice(n).then(data => { dv1 = data.dice.value; console.log(dv1) })
+  bot.sendDice(n).then(data => { dv2 = data.dice.value; console.log(dv2) })
+    .then(d => {
+      if (dv1 + dv2 === 7) {
+        console.log("W")
+      }
+      else {
+        console.log('L')
+      }
+    })
+}
+
 
 bot.on('polling_error', listen => {
   console.log(listen)
